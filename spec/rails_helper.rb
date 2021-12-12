549692e519575783813422da88033ae342900b52
Mateusz Tomczak <kittz.esport@gmail.com>
2021-12-12 19:15:56 +0100

    far[13] - configure RSpec - add random spec order, fail fast option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
+  config.fail_fast = 1
+  config.order = :random
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
